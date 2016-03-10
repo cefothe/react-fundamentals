0dcb79b99eb3c115be1248b2b2580b88ce0299bc
Stefan Angelov <stefanangelov@deltasource.eu>
2016-03-10 17:40:31 +0200

    propertyes

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import React from 'React';
+import React from 'react';
+import ReactDOM from 'react-dom'
 class App extends React.Component{
 	render(){
-         return (
-         	<div>
-	         	<h1>Hello World</h1>
-	         	<b>Bold</b>
-         	</div>
-         	);
+		let txt =this.props.txt
+         return <h1>{txt}</h1>;
 	}
 }
- 
-export default App
+
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps ={
+	txt: 'This is the defaul value'
+}
+ ReactDOM.render(
+ 	<App cat={6} />,
+  	document.getElementById('app')
+  )
