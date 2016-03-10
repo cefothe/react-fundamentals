33394f39f1408d58a5a1c258b7632ca213431500
Stefan Angelov <stefanangelov@deltasource.eu>
2016-03-10 17:23:03 +0200

    another options

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'React';
-class App extends React.Component{
-	render(){
-         return React.createElement('h1',null,'Hello Guys')
-	}
-}
+// class App extends React.Component{
+// 	render(){
+//          return React.createElement('h1',null,'Hello Guys')
+// 	}
+// }
 
+const App = () => <h1>Hello EggHeads</h1>
 export default App
